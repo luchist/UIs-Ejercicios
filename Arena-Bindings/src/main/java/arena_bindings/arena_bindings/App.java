4884804d53ac3e835bd172fea91a765e83bc61fb
Luis Coronel <lucho_9292@live.com>
2018-05-02 02:09:57 -0300

    Agrego el proyecto base
    Modifico .gitignore

---
+++ Arena-Bindings/src/main/java/arena_bindings/arena_bindings/App.java
@@ -0,0 +1,13 @@
+package arena_bindings.arena_bindings;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
